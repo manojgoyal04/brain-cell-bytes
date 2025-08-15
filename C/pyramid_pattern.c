49722b42449f08c419372a2a88a67b289fd977c6
Mitansh Goyal <mitanshgoyal18@gmail.com>
2025-08-15 10:02:08 +0530

    changed pyramid file

--- C/pyramid_pattern.c
+++ C/pyramid_pattern.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main (){
     int limit;
-    printf("Please enter the number of lines in the pyramid ");
+    printf("Please enter the number of rows in the pyramid: ");
     scanf("%d",& limit);
     for(int i=1; i<= limit; i++){
         for (int j = limit; j>i; j--){
